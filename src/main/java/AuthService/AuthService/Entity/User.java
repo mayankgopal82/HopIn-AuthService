56d4436f6f78fad2d6efeb65adb30f99f8d26808
Mayank Gopal <mayankgopal05@gmail.com>
2025-10-01 03:33:36 +0530

    added getters and setters for User entity- temporary fix

--- src/main/java/AuthService/AuthService/Entity/User.java
+++ src/main/java/AuthService/AuthService/Entity/User.java
@@ -3,10 +3,17 @@ package AuthService.AuthService.Entity;
 import AuthService.AuthService.Enum.Role;
 import AuthService.AuthService.Enum.Status;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.util.UUID;
 
 @Entity
 @Data
@@ -15,11 +22,71 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class User {
 
-    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String firstName;
+    private String lastName;
     private String email;
     private String mobile;
     private String password;// hashed password
     private Role role;
-    private Status status;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
 }
