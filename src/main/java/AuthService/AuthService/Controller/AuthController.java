d65b3357277b82a2746debf1a99ecb21e95afc86
Mayank Gopal <mayankgopal05@gmail.com>
2025-10-01 03:35:39 +0530

    generic change

--- src/main/java/AuthService/AuthService/Controller/AuthController.java
+++ src/main/java/AuthService/AuthService/Controller/AuthController.java
@@ -2,7 +2,9 @@ package AuthService.AuthService.Controller;
 
 import AuthService.AuthService.DTO.SignupPassengerRequestDto;
 import AuthService.AuthService.DTO.SignupPassengerResponseDto;
+import AuthService.AuthService.Service.AuthService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
+    @Autowired
+    AuthService authService;
     @PostMapping("/signup/passenger")
     public ResponseEntity<SignupPassengerResponseDto> signUp(@Valid @RequestBody SignupPassengerRequestDto request){
+
+        authService.signupPassenger(request);
+//        return ResponseEntity();
         return null;
+
     }
 
 }
--- src/main/java/AuthService/AuthService/Service/AuthService.java
+++ src/main/java/AuthService/AuthService/Service/AuthService.java
@@ -1,11 +1,43 @@
 package AuthService.AuthService.Service;
 
+import AuthService.AuthService.Client.PassengerClient;
+import AuthService.AuthService.DTO.SignupPassengerRequestDto;
+import AuthService.AuthService.Entity.User;
+import AuthService.AuthService.Enum.Role;
+import AuthService.AuthService.Enum.Status;
+import AuthService.AuthService.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class AuthService {
 
-    public void signupPassenger(){
-add 
+    @Autowired
+    PassengerClient passengerClient;
+
+    @Autowired
+    UserRepository userRepo;
+
+
+    public void signupPassenger(SignupPassengerRequestDto request){
+
+        request.setRole(Role.PASSENGER);
+        passengerClient.registerPassenger(request);
+        //need to do mapping here using mapstruct --for now
+        //manually mapping
+//        User user=   User.builder().role(request.getRole()).email(request.getEmail()).firstName(request.getFirstName())
+//                        .lastName(request.getLastName()).mobile(request.getMobile()).password(request.getPassword()).build();
+        // Mapping using setters instead of builder
+        User user = new User();
+        user.setRole(request.getRole());
+        user.setEmail(request.getEmail());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setMobile(request.getMobile());
+        user.setPassword(request.getPassword());
+        userRepo.save(user);
+
     }
 }
