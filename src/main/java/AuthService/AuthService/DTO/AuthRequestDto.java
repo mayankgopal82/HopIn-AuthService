7c8c87f6a185fb94bdc263ff7d71528308b4c79e
Mayank Gopal <mayankgopal05@gmail.com>
2025-11-20 01:39:17 +0530

    auth changes - signin

---
+++ src/main/java/AuthService/AuthService/DTO/AuthRequestDto.java
@@ -0,0 +1,34 @@
+package AuthService.AuthService.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthRequestDto {
+
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+}
--- src/main/java/AuthService/AuthService/config/SecurityConfig.java
+++ src/main/java/AuthService/AuthService/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package AuthService.AuthService.config;
 
+import AuthService.AuthService.Service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,10 +22,13 @@ import org.springframework.security.web.SecurityFilterChain;
 //@EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.ignoringRequestMatchers("/api/v1/auth/signup/**"))
-            .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/signup/**").permitAll()
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/api/v1/auth/signup/**","/api/v1/auth/signin/**"))
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/signup/**","/api/v1/auth/signin/**").permitAll()
                 .anyRequest().authenticated());
         return http.build();
     }
@@ -27,4 +37,23 @@ public class SecurityConfig {
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    /*AuthenticationProvicer object is responsible for verifying credentials
+      - daoAuthenticationProvider is one of the strategies to verify credentials which
+        uses local db,fetched the user by email or username
+    * */
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider= new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+       return config.getAuthenticationManager();
+    }
+
+
 }
